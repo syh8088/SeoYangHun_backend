96f7f8c9b05a727f69779585acf2398891c5616c
syh8088 <syh8088@gmail.com>
2025-01-25 16:31:15 +0900

    ✨ SecurityConfig 추가
    SecurityConfig 추가

---
+++ src/main/java/com/wirebarley/global/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.wirebarley.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain restSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+        ;
+
+        return http.build();
+    }
+}
